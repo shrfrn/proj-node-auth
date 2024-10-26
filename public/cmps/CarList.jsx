1955b9613b16141af91df9b6b3e4ae66841336f4
shrfrn <shrfrn@gmail.com>
2024-10-26 18:14:20 +0300

    Revert CarList to local user service

--- public/cmps/CarList.jsx
+++ public/cmps/CarList.jsx
@@ -1,35 +1,37 @@
 const { Link } = ReactRouterDOM
 
-import { userService } from "../services/user.service.js";
-import { CarPreview } from "./CarPreview.jsx";
+import { userService } from '../services/user.service.local.js'
+import { CarPreview } from './CarPreview.jsx'
 
 export function CarList({ cars, onRemoveCar }) {
+	const user = userService.getLoggedinUser()
 
-    const user = userService.getLoggedinUser()
+	function isOwner(car) {
+		if (!user) return false
+		if (!car.owner) return true
+		return user.isAdmin || car.owner._id === user._id
+	}
 
-    function isOwner(car) {
-        if (!user) return false
-        if (!car.owner) return true
-        return  user.isAdmin || car.owner._id === user._id
-    }
-
-    return (
-        <ul className="car-list">
-            {cars.map(car =>
-                <li key={car._id}>
-                    <CarPreview car={car} />
-                    <section>
-                        <button><Link to={`/car/${car._id}`}>Details</Link></button>
-                        {
-                            isOwner(car) && 
-                            <div>
-                                <button onClick={() => onRemoveCar(car._id)}>Remove Car</button>
-                                <button><Link to={`/car/edit/${car._id}`}>Edit</Link></button>
-                            </div>
-                        }
-                    </section>
-                </li>
-            )}
-        </ul>
-    )
+	return (
+		<ul className="car-list">
+			{cars.map(car => (
+				<li key={car._id}>
+					<CarPreview car={car} />
+					<section>
+						<button>
+							<Link to={`/car/${car._id}`}>Details</Link>
+						</button>
+						{isOwner(car) && (
+							<div>
+								<button onClick={() => onRemoveCar(car._id)}>Remove Car</button>
+								<button>
+									<Link to={`/car/edit/${car._id}`}>Edit</Link>
+								</button>
+							</div>
+						)}
+					</section>
+				</li>
+			))}
+		</ul>
+	)
 }
