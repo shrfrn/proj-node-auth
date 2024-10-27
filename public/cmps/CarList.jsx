a2c01328a16e489ed98ba3abb536e471ed74a7e3
shrfrn <shrfrn@gmail.com>
2024-10-27 19:53:57 +0200

    Remove auth from UI - todo inclass instead

--- public/cmps/CarList.jsx
+++ public/cmps/CarList.jsx
@@ -6,32 +6,22 @@ import { CarPreview } from './CarPreview.jsx'
 export function CarList({ cars, onRemoveCar }) {
 	const user = userService.getLoggedinUser()
 
-	function isOwner(car) {
-		if (!user) return false
-		if (!car.owner) return true
-		return user.isAdmin || car.owner._id === user._id
-	}
-
-	return (
-		<ul className="car-list">
-			{cars.map(car => (
-				<li key={car._id}>
-					<CarPreview car={car} />
-					<section>
-						<button>
-							<Link to={`/car/${car._id}`}>Details</Link>
-						</button>
-						{isOwner(car) && (
-							<div>
-								<button onClick={() => onRemoveCar(car._id)}>Remove Car</button>
-								<button>
-									<Link to={`/car/edit/${car._id}`}>Edit</Link>
-								</button>
-							</div>
-						)}
-					</section>
-				</li>
-			))}
-		</ul>
-	)
+	return <ul className="car-list">
+        {cars.map(car => (
+            <li key={car._id}>
+                <CarPreview car={car} />
+                <section className="actions">
+                    <button>
+                        <Link to={`/car/${car._id}`}>Details</Link>
+                    </button>
+                    <div>
+                        <button onClick={() => onRemoveCar(car._id)}>Remove Car</button>
+                        <button>
+                            <Link to={`/car/edit/${car._id}`}>Edit</Link>
+                        </button>
+                    </div>
+                </section>
+            </li>
+        ))}
+    </ul>
 }
